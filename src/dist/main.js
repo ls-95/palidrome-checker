9d6631349920b3d6179120157fddbb047d4ec07c
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-07-29 15:44:15 +0200

    Add palindrome checker functionality

--- src/dist/main.js
+++ src/dist/main.js
@@ -1,1 +1,18 @@
-console.log("hello");
+var textInput = document.getElementById("text-input");
+var checkBtn = document.getElementById("check-btn");
+var result = document.getElementById("result");
+var handleClick = function () {
+    var cleaned = textInput.value
+        .replace(/[^a-zA-Z]/g, "")
+        .toLowerCase();
+    if (textInput.value.trim() === "") {
+        alert("Please enter a word...");
+        return;
+    }
+    var reversedWord = cleaned.split("").reverse().join("");
+    console.log(reversedWord);
+    reversedWord === cleaned
+        ? (result.innerHTML = "\"<strong>".concat(textInput.value, "</strong>\" is a palindrome."))
+        : (result.innerHTML = "\"<strong>".concat(textInput.value, "</strong>\" is NOT a palindrome."));
+};
+checkBtn === null || checkBtn === void 0 ? void 0 : checkBtn.addEventListener("click", handleClick);
--- src/dist/main.ts
+++ src/dist/main.ts
@@ -1,3 +1,23 @@
-const textInput = document.getElementById("text-input");
-const checkBtn = document.getElementById("check-btn");
-const result = document.getElementById("result");
+const textInput = document.getElementById("text-input") as HTMLInputElement;
+const checkBtn = document.getElementById("check-btn") as HTMLButtonElement;
+const result = document.getElementById("result") as HTMLDivElement;
+
+const handleClick = (): void => {
+  const cleaned: string = textInput.value
+    .replace(/[^a-zA-Z]/g, "")
+    .toLowerCase();
+
+  if (textInput.value.trim() === "") {
+    alert("Please enter a word...");
+    return;
+  }
+
+  const reversedWord: string = cleaned.split("").reverse().join("");
+  console.log(reversedWord);
+
+  reversedWord === cleaned
+    ? (result.innerHTML = `"<strong>${textInput.value}</strong>" is a palindrome.`)
+    : (result.innerHTML = `"<strong>${textInput.value}</strong>" is NOT a palindrome.`);
+};
+
+checkBtn?.addEventListener("click", handleClick);
